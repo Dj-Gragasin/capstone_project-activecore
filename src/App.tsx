fce70d828b03c5626d05a0f5eb53acda7efb01ac
Dj-Gragasin <djgragasin18@gmail.com>
2026-07-05 20:06:42 +0800

    Fix: move forgot/reset routes outside Home render (fix JSX syntax)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from "react";
-import {
-  IonApp,
-  IonRouterOutlet,
-  IonSplitPane,
-  setupIonicReact,
-} from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import { Redirect, Route } from "react-router-dom";
-
-/* Core Ionic CSS */
-import "@ionic/react/css/core.css";
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
+                return (
+                  <Redirect
+                    to={
+                      auth.role === "admin" ? "/admin" : "/member"
+                    }
+                  />
+                );
 import "@ionic/react/css/typography.css";
 import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
+
+            {/* Forgot / Reset (public) */}
+            <Route exact path="/forgot-password" component={ForgotPassword} />
+            <Route exact path="/reset-password" component={ResetPassword} />
 import "@ionic/react/css/display.css";
 
 /* Theme variables */
